d2f32aaadcc9f48957570623412e3acc63efa7ec
MissTitanK3 <tianna.m.mccoy@gmail.com>
2021-05-03 10:07:28 -0700

    Added data for newsfeed, started on about page.

--- client/src/view/About.js
+++ client/src/view/About.js
@@ -3,7 +3,41 @@ import React from 'react'
 export default function About() {
   return (
     <div>
-      about
+      <div className="img"></div>
+      <div className="title"></div>
+      <div className="intro"></div>
+      <div className="details">
+        <div className="img"></div>
+        <div className="title"></div>
+        <div className="txtbdy"></div>
+        <div className="img"></div>
+      </div>
+      <div className="board">
+        <div className="intro"></div>
+        <div className="positions"></div>
+        <div className="outro"></div>
+      </div>
+      <div className="involved">
+        <div className="apply">
+          <div className="desc"></div>
+          <div className="links">
+            <div className="eapp"></div>
+            <div className="download"></div>
+            <div className="email"></div>
+          </div>
+        </div>
+        <div className="subcom">
+          <div className="email"></div>
+        </div>
+        <div className="volunteer">
+          <div className="email"></div>
+        </div>
+      </div>
+      <div className="docs">
+        <div className="budget"></div>
+        <div className="board"></div>
+        <div className="committee"></div>
+      </div>
     </div>
   )
 }
--- client/src/view/News.js
+++ client/src/view/News.js
@@ -1,9 +1,34 @@
+import axios from 'axios';
 import React from 'react'
+import { useQuery } from 'react-query';
+import { useWPPostStore } from '../api/store';
 
 export default function News() {
+  const WPPost = useWPPostStore(state => state.someData)
+  const addWPPost = useWPPostStore(state => state.addData)
+  const updateDB = '<div>Please Update Database</div>'
+  console.log(WPPost)
+
+
+  function useBusiness() {
+    return useQuery("posts", async () => {
+      const { data } = await axios.get(
+        "https://oldetownarvada.org/wp-json/wp/v2/newsfeed"
+      );
+      addWPPost(data)
+    });
+  }
+  // TODO this doesnt work in a useEffect, debug
+  useBusiness()
+
   return (
     <div>
-      News
+      {WPPost.map((post) => (
+        <>
+          <h2>{post?.title.rendered}</h2>
+          <p>{post?.acf?.description ? post?.acf?.description : updateDB}</p>
+        </>
+      ))}
     </div>
   )
 }
